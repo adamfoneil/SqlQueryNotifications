17f64dd72626d48c8ec8e646ddadc378472542ff
Adam O'Neil <adamosoftware@gmail.com>
2021-01-20 08:35:58 -0500

    blob example working

--- Testing/QueryNotifications.cs
+++ Testing/QueryNotifications.cs
@@ -1,12 +1,19 @@
 using Dapper;
+using Microsoft.Extensions.Configuration;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using SqlQueryNotifications;
 using SqlQueryNotifications.Models;
 using SqlServer.LocalDb;
 using SqlServer.LocalDb.Models;
+using System;
 using System.Collections.Generic;
 using System.Net;
 using System.Net.Mail;
+using Azure.Storage.Blobs;
+using System.Text.Json;
+using System.IO;
+using System.Text;
+using Azure.Storage.Blobs.Models;
 
 namespace Testing
 {
@@ -58,19 +65,58 @@ namespace Testing
             {
                 Queries = new Query[]
                 {
-                    new Query()
-                    {
-                        Subject = "Sample query notification",
-                        Recipients = new string[] { "adamosoftware@gmail.com" },
-                        BodyText = "This is a sample query notification. There should not be any rows in SomeTable WHERE Quantity is greater than 10.",
-                        Sql = "SELECT * FROM [dbo].[SomeTable] WHERE [Quantity]>10",
-                        SendRule = TriggerSendRule.IfAny
-                    }
+                    SampleQuery()
                 }
             };
 
             var service = new SqlQueryNotificationService(smtp, querySource, null);
             service.ExecuteAsync().Wait();
         }
+
+        [TestMethod]
+        public void BlobDataResult()
+        {
+            var config = ConfigHelper.GetConfig();
+
+            var smtp = new SmtpClient(
+                config["SendGrid:Server"],
+                int.Parse(config["SendGrid:Port"]));
+
+            smtp.Credentials = new NetworkCredential("apikey", config["SendGrid:ApiKey"]);
+
+            var querySource = new BlobQuerySource(
+                config["Azure:ConnectionString"], 
+                LocalDb.GetConnectionString(DbName), 
+                "noreply@aosoftware.net", 
+                config["Azure:ContainerName"], 
+                config["Azure:BlobPrefix"]);
+
+            CreateSampleQueryBlob(config);
+
+            var service = new SqlQueryNotificationService(smtp, querySource, null);
+            service.ExecuteAsync().Wait();
+        }
+
+        private void CreateSampleQueryBlob(IConfigurationRoot config)
+        {
+            var blobClient = new BlobClient(config["Azure:ConnectionString"], config["Azure:ContainerName"], "Queries/SampleQuery.json");
+            var json = JsonSerializer.Serialize(SampleQuery());
+            var bytes = Encoding.UTF8.GetBytes(json);
+
+            using (var ms = new MemoryStream(bytes))
+            {
+                blobClient.DeleteIfExists();
+                blobClient.Upload(ms);
+            }            
+        }
+
+        private Query SampleQuery() => new Query()
+        {
+            Subject = "Sample query notification",
+            Recipients = new string[] { "adamosoftware@gmail.com" },
+            BodyText = "This is a sample query notification. There should not be any rows in SomeTable WHERE Quantity is greater than 10.",
+            Sql = "SELECT * FROM [dbo].[SomeTable] WHERE [Quantity]>10",
+            SendRule = TriggerSendRule.IfAny
+        };
     }
 }
